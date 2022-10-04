0ffb40bd2a76e1fdb546275fc02f40a4476f957b
伊岑 鄭 <molly021456@gmail.com>
2022-10-04 14:17:54 +0800

    加入專案檔案。

---
+++ prjWebApplication1/Controllers/RoomController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using prjWebApplication1.Models;
+using prjWebApplication1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjWebApplication1.Controllers
+{
+    public class RoomController : Controller
+    {
+        private IWebHostEnvironment _enviro;
+        public RoomController(IWebHostEnvironment p)
+        {
+            _enviro = p;
+        }
+
+        public IActionResult List(CKeywordViewModel model)
+        {
+            MingSuContext db = new MingSuContext();
+            IEnumerable<Room> datas = null;
+            if (string.IsNullOrEmpty(model.txtKeyword))
+
+                datas = from r in db.Rooms
+                        select r;
+            else
+                datas = db.Rooms.Where(p => p.RoomName.Contains(model.txtKeyword)
+                || p.RoomPrice.ToString().Contains(model.txtKeyword)
+                || p.RoomIntrodution.Contains(model.txtKeyword)
+                || p.MemberId.ToString().Contains(model.txtKeyword)
+                || p.RoomstatusId.ToString().Contains(model.txtKeyword)
+                || p.Address.Contains(model.txtKeyword)
+                || p.CreateDate.ToString().Contains(model.txtKeyword)
+                || p.FImagePath.Contains(model.txtKeyword));
+            return View(datas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Room p)
+        {
+            MingSuContext db = new MingSuContext();
+            db.Rooms.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                MingSuContext db = new MingSuContext();
+                Room r = db.Rooms.FirstOrDefault(t => t.RoomId == id);
+                if (r != null)
+                {
+                    db.Rooms.Remove(r);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("List");
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+                MingSuContext db = new MingSuContext();
+                Room c = db.Rooms.FirstOrDefault(t => t.RoomId ==id);
+                if (c != null)
+                    return View(c);
+            }
+            return RedirectToAction("List");
+        }
+        [HttpPost]
+        public ActionResult Edit(CRoomViewModel inProd)
+        {
+            MingSuContext db = new MingSuContext();
+            Room c = db.Rooms.FirstOrDefault(t => t.RoomId == inProd.RoomID);
+            if (c != null)
+            {
+                if (inProd.photo != null)
+                {
+                    string pName = Guid.NewGuid().ToString() + ".jpg";
+                    c.FImagePath = pName;
+                    string path = _enviro.WebRootPath + "/images/" + pName;
+                    inProd.photo.CopyTo(new FileStream(path, FileMode.Create));
+                }
+                c.RoomName = inProd.RoomName;
+                c.RoomPrice = inProd.RoomPrice;
+                c.RoomIntrodution = inProd.RoomIntrodution;
+                c.Address = inProd.Address;
+                c.CreateDate = inProd.CreateDate;
+                c.MemberId = inProd.MemberId;
+                c.RoomstatusId = inProd.RoomstatusId;
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+
+
+
+
+
+
+
+    }
+    }
---
+++ prjWebApplication1/ViewModels/CRoomViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using prjWebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjWebApplication1.ViewModels
+{
+    public class CRoomViewModel
+    {
+        private Room _room;
+        public Room room
+        {
+            get { return _room; }
+            set { _room = value; }
+        }
+        public CRoomViewModel()
+        {
+            _room = new Room();
+        }
+        public int RoomID
+        {
+            get { return _room.RoomId; }
+            set { _room.RoomId = value; }
+        }
+        public string RoomName
+        {
+            get { return _room.RoomName; }
+            set { _room.RoomName = value; }
+        }
+        public decimal RoomPrice
+        {
+            get { return _room.RoomPrice; }
+            set { _room.RoomPrice = value; }
+        }
+        public string RoomIntrodution
+        {
+            get { return _room.RoomIntrodution; }
+            set { _room.RoomIntrodution = value; }
+        }
+        public string Address
+        {
+            get { return _room.Address; }
+            set { _room.Address = value; }
+        }
+        public int MemberId
+        {
+            get { return _room.MemberId; }
+            set { _room.MemberId = value; }
+        }
+        public int RoomstatusId
+        {
+            get { return _room.RoomstatusId; }
+            set { _room.RoomstatusId = value; }
+        }
+
+        public DateTime? CreateDate
+        {
+            get { return _room.CreateDate; }
+            set { _room.CreateDate = value; }
+        }
+        public string FImagePath
+        {
+            get { return _room.FImagePath; }
+            set { _room.FImagePath = value; }
+        }
+        public IFormFile photo { get; set; }
+
+
+
+    }
+}
